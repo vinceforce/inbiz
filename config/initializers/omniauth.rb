f9f61ff9ee90a8fe16f1871157c93f077bdd37ff
Vincent Force <vforce@t2bh.fr>
2017-03-19 15:58:40 +0100

    Cle Google compte vforce.dev

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '368305440792-d2qe1foo5qojlb3pf68q6f2g7t874094.apps.googleusercontent.com', 'YHcq8eYFXMZAA0QfRRyJcKtR', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '368305440792-d2qe1foo5qojlb3pf68q6f2g7t874094.apps.googleusercontent.com', 'AIzaSyDycr1a3nJG0eZSwLXKScABGDmRggKNgR4', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
